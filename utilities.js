d3e7e71eaffbd76b86abf74371de66cb96b7c304
Duong Dinh Cuong <cuong3ihut@gmail.com>
2020-05-17 14:59:43 +0200

    Adding utitlies library support SHA256 and download file...

---
+++ utilities.js
@@ -0,0 +1,64 @@
+const http = require('http')
+const crypto = require('crypto')
+const fs = require('fs')
+
+const getOutputKeyValue = function(outputs, propKey) {
+    var result = null
+    outputs.some(function(output) {
+        if (output.OutputKey === propKey) {
+            result = output.OutputValue
+            return true
+        }
+        return false
+    })
+    return result
+}
+
+const getSha256FileInHex = function(filePath) {
+    const data = fs.readFileSync(filePath)
+    return crypto.createHash('sha256').update(data).digest('hex')
+}
+
+const getSha256FileInBase64 = function(filePath) {
+    const data = fs.readFileSync(filePath)
+    return crypto.createHash('sha256').update(data).digest('base64')
+}
+
+const downloadFileFromUrl = function(url, dest) {
+    return new Promise((resolve, reject) => {
+        const file = fs.createWriteStream(dest, { flags: "wx" })
+        const request = http.get(url, response => {
+            if (response.statusCode === 200) {
+                response.pipe(file)
+            } else {
+                file.close()
+                fs.unlink(dest, () => {})
+                reject({ message: `Server responded with ${response.statusCode}: ${response.statusMessage}` });
+            }
+        })
+        request.on("error", err => {
+            file.close()
+            fs.unlink(dest, () => {})
+            reject(err)
+        })
+        file.on("finish", () => {
+            resolve()
+        })
+        file.on("error", err => {
+            file.close()
+            if (err.code === "EEXIST") {
+                reject({ message: "File already exists" });
+            } else {
+                fs.unlink(dest, () => {})
+                reject(err)
+            }
+        })
+    })
+}
+
+module.exports = {
+    getOutputKeyValue,
+    getSha256FileInHex,
+    getSha256FileInBase64,
+    downloadFileFromUrl
+}

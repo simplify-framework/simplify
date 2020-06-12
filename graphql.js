f8add9d1c5405ae0a4787c2b2095f6681f2a6144
Duong Dinh Cuong <cuong3ihut@gmail.com>
2020-06-12 22:35:16 +0200

    Update retry after failed for workflow engine

--- graphql.js
+++ graphql.js
@@ -15,19 +15,33 @@ class StateExecution {
     runNextExecution({ args, context }, stateObject, states) {
         const _thisFunction = this
         return new Promise((resolve, reject) => {
-            const stateFunction = require(path.join(this.executionPath, stateObject.Run)).handler
+            const _stateFunction = require(path.join(_thisFunction.executionPath, stateObject.Run)).handler
             _thisFunction.verbose("StateExecution:RUN_CONTEXT", args, context)
-            stateFunction(args, context, function (err, data) {
+            _stateFunction(args, context, function (err, data) {
                 if (err && stateObject.Other !== "DONE") {
                     context.dataContext = data
                     context.errorContext = err
-                    _thisFunction.verbose("StateExecution:OTHER_CONTEXT", data)
-                    const nextState = states.find(state => state.Run === stateObject.Other)
-                    if (!nextState) reject({ message: `The execution state is not available: ${stateObject.Other}` })
-                    _thisFunction.runNextExecution({ args, context }, nextState, states).then(data => resolve(data)).catch(err => reject(err))
+                    context.retryState = context.retryState || stateObject.Retry
+                    if (context.retryState && context.retryState > 0) {
+                        context.retryState--
+                        _thisFunction.verbose("StateExecution:RETRY_CONTEXT count=", context.retryState)
+                        _thisFunction.runNextExecution({ args, context }, stateObject, states).then(data => resolve(data)).catch(err => reject(err))
+                    } else {
+                        _thisFunction.verbose("StateExecution:OTHER_CONTEXT", data)
+                        const nextState = states.find(state => state.Run === stateObject.Other)
+                        if (!nextState) reject({ message: `The execution state is not available: ${stateObject.Other}` })
+                        _thisFunction.runNextExecution({ args, context }, nextState, states).then(data => resolve(data)).catch(err => reject(err))
+                    }
                 } else if (err && stateObject.Other === "DONE") {
-                    _thisFunction.verbose("StateExecution:ERROR_CONTEXT", data)
-                    reject(data)
+                    context.retryState = context.retryState || stateObject.Retry
+                    if (context.retryState && context.retryState > 0) {
+                        context.retryState--
+                        _thisFunction.verbose("StateExecution:RETRY_CONTEXT count=", context.retryState)
+                        _thisFunction.runNextExecution({ args, context }, stateObject, states).then(data => resolve(data)).catch(err => reject(err))
+                    } else {
+                        _thisFunction.verbose("StateExecution:ERROR_CONTEXT", data)
+                        reject(data)
+                    }
                 } else if (!err && stateObject.Next !== "DONE") {
                     context.dataContext = data
                     context.errorContext = err
